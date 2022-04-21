7ee1f5d1e1ed6baa1b2967858991947b8a99968f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-04-21 14:04:54 +0200

    added documentation

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 
+/**
+ * Permet de modifier la configuration de PHP
+ * @see https://www.php.net/manual/en/function.ini-set.php
+ */
 ini_set("display_errors", 1);
 
+/**
+ * Permet d'éviter que les erreurs, avertissements et autres soient masqués
+ * @see https://www.php.net/manual/en/function.error-reporting.php
+ */
 error_reporting(E_ALL);
 
+/**
+ * La route ré-écrite via la règle de ré-écriture d'URL du fichier .htaccess
+ * @see https://www.php.net/manual/en/language.operators.comparison.php
+ * @see https://www.php.net/manual/en/function.isset.php
+ */
 $route = isset($_REQUEST["route"]) ? $_REQUEST["route"] : "undefined";
 
 echo $route;
